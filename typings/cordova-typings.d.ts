052e42dc8568176fca8b01e7865c765776adf1e7
turagabhupathi <turagabhupathi@gmail.com>
2020-02-14 14:27:10 +0530

    test folder

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,8 @@
+
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileTransfer.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileSystem.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Toast.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\NetworkInformation.d.ts"/>
